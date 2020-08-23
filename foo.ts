6002c20ae7bfa7a6a36ddaaf1db7282086b90042
Jonathan Alipate <j.alipate@gmail.com>
2020-08-23 21:52:22 +1200

    string constraint

--- foo.ts
+++ foo.ts
@@ -1,8 +1,7 @@
 
-function foo(bar?: "car" | "bus" | number) {
+function foo(bar: string) {
     return "Hello, " + bar;
 }
 
 let baz = "ABC";
-console.log(foo())
-console.log(foo('car'));
+console.log(foo(baz));
